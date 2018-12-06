2640f072548210a5ce49952ff583003b25686306
MrZhangzhg <zhangzg@tedu.cn>
2018-12-06 18:14:34 +0800

    1807 py1 day1 18pm

--- nsd1807/python1/day01/hello.py
+++ nsd1807/python1/day01/hello.py
@@ -1,1 +1,37 @@
-print()
+# print('Hello World!')
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+#
+# print('abc')
+#
+# a = 3
+# b = 4
+# x = 3; y = 4
+
+# print('Hello World')
+# # print语句各项之间默认用空格分隔
+# print('Hello', 'World')
+# # print语句各项之间也可以自定义分隔符
+# print('Hello', 'world', sep='***')
+# # 字符串可以使用+进行拼接
+# print('Hello' + 'world')
+# # print语句默认最后会加入一个回车\n，可以用end来自定义结束
+# print('Hello World', end='AAA')
+############################################
+
+a = input('number: ')
+print(a)
+# b = a + 10     # input读入的数据都是字符型，字符不能和数字相加
+print(int(a) + 10)   # 将a转成整数后和10相加，再打印出出结果
+print(a + str(10))   # 将10转成字符串后和a拼接，再打印出出结果
+
+
+
+
+
+
+
+
+
