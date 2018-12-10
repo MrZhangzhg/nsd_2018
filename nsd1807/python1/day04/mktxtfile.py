68e9f617a51a9e828a0fd2db9b51cdcda7f139c8
MrZhangzhg <zhangzg@tedu.cn>
2018-12-10 12:01:30 +0800

    1807 py1 day4 12am

---
+++ nsd1807/python1/day04/mktxtfile.py
@@ -0,0 +1,30 @@
+import os
+
+def get_fname():
+    while True:
+        fname = input('filename: ')
+        if not os.path.exists(fname):
+            break
+        print('文件已存在，请重试')
+
+    return fname
+
+def get_content():
+    content = []
+    while True:
+        line = input('("end" to quit)> ')
+        if line == 'end':
+            break
+        content.append(line)
+
+    return content
+
+def wfile(fname, content):
+    with open(fname, 'w') as fobj:
+        fobj.writelines(content)
+
+if __name__ == '__main__':
+    fname = get_fname()
+    content = get_content()
+    content = [line + '\n' for line in content]
+    wfile(fname, content)
