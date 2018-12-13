7eed09af88a71638930ba8a323fbb1d4a1b8b3ca
MrZhangzhg <zhangzg@tedu.cn>
2018-12-13 17:54:08 +0800

    1807 py2 day2 18:01pm

--- nsd1807/python2/day02/file_generator.py
+++ nsd1807/python2/day02/file_generator.py
@@ -2,11 +2,11 @@ def file_block(fobj):
     content = []
     for line in fobj:
         content.append(line)
-        if len(content) == 10:
+        if len(content) == 10:   # 10行生成一次
             yield content
-            content.clear()
+            content.clear()      # 生成数据交给使用者后清空，以便存下10行
 
-    if content:
+    if content:    # 如果循环结束后还有数据，生成最后不到10行的内容
         yield content
 
 
