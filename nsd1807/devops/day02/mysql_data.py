a7bea865ca42213e6c7e051a72c7fd2b13b59c09
MrZhangzhg <zhangzg@tedu.cn>
2018-12-19 11:32:32 +0800

    1807 devops day2 11:30am

--- nsd1807/devops/day02/mysql_data.py
+++ nsd1807/devops/day02/mysql_data.py
@@ -9,15 +9,19 @@ conn = pymysql.connect(
     charset='utf8'
 )
 cursor = conn.cursor()
-# create_dep = '''CREATE TABLE departments(dep_id INT,
-# dep_name VARCHAR(20) UNIQUE NOT NULL, PRIMARY KEY(dep_id))
-# '''
-create_emp = '''CREATE TABLE employess(emp_id INT,
+create_dep = '''CREATE TABLE departments(dep_id INT,
+dep_name VARCHAR(20) UNIQUE NOT NULL, PRIMARY KEY(dep_id))
+'''
+create_emp = '''CREATE TABLE employees(emp_id INT,
 emp_name VARCHAR(20), gender VARCHAR(6), email VARCHAR(50),
 phone CHAR(11), dep_id INT, PRIMARY KEY(emp_id),
 FOREIGN KEY(dep_id) REFERENCES departments(dep_id))'''
-# cursor.execute(create_dep)
+create_sal = '''CREATE TABLE salary(auto_id INT, date DATE,
+emp_id INT, basic INT, awards INT, PRIMARY KEY(auto_id),
+FOREIGN KEY(emp_id) REFERENCES employees(emp_id))'''
+cursor.execute(create_dep)
 cursor.execute(create_emp)
+cursor.execute(create_sal)
 conn.commit()
 cursor.close()
 conn.close()
