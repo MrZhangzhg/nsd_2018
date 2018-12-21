820d5f60627a369454dca3740910e163eb973b74
MrZhangzhg <zhangzg@tedu.cn>
2018-12-21 18:02:45 +0800

    1807 devops day3 18pm

---
+++ nsd1807/devops/day03/rcmd.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import paramiko
+import getpass
+import sys
+import os
+import threading
+
+def rcmd(host, passwd, cmd, user='root', port=22):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username=user, password=passwd, port=port)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    out = stdout.read()
+    err = stderr.read()
+    if out:
+        print('[%s] OUT:\n%s' % (host, out.decode()))
+    if err:
+        print('[%s] ERROR:\n%s' % (host, err.decode()))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: %s ipfile "command"' % sys.argv[0])
+        exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print('No such file:', sys.argv[1])
+        exit(2)
+    ipfile = sys.argv[1]
+    command = sys.argv[2]
+    passwd = getpass.getpass()
+    with open(ipfile) as fobj:
+        for line in fobj:
+            ip = line.strip()  # 移除\n
+            # rcmd(ip, passwd, command)
+            t = threading.Thread(target=rcmd, args=(ip, passwd, command))
+            t.start()
