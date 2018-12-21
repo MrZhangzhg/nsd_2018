daf89979b35e081eeb4ac211af37ce471e8591c2
MrZhangzhg <zhangzg@tedu.cn>
2018-12-21 11:51:59 +0800

    1807 devops day3 12am

--- nsd1807/devops/day03/get_resource.py
+++ nsd1807/devops/day03/get_resource.py
@@ -13,21 +13,22 @@ def download(url, fname):
 
 def find_patt(fname, patt, code='utf8'):
     cpatt = re.compile(patt)
-    result = []
-    with open(fname, encoding=code) as fobj:
+    result = []  # 把所有的匹配存入该列表
+    with open(fname, encoding=code) as fobj:  # 打开文件时，可以指定字符集
         for line in fobj:
-            match_objs = cpatt.finditer(line)
+            match_objs = cpatt.finditer(line)  # 找到一行中的多个模式
             for m in match_objs:
-                result.append(m.group())
+                result.append(m.group())  # 将找到的内容追加到列表
     return result
 
 
 if __name__ == '__main__':
     net163 = 'http://www.163.com'
     file163 = '/tmp/163.html'
-    download(net163, file163)
+    download(net163, file163)  # 下载网易首页
+    # 编写图片url的正则表达式
     img_patt = '(http|https)://[-\w./]+(\.jpg|\.jpeg|\.png|\.gif)'
-    img_list = find_patt(file163, img_patt, 'gbk')
+    img_list = find_patt(file163, img_patt, 'gbk')  # 获取所有的图片URL
     print(img_list)
 
 
