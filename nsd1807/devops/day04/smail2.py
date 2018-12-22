8253c4c92047007a4e04aea6e4546bf2ee30c7a0
MrZhangzhg <zhangzg@tedu.cn>
2018-12-22 11:53:50 +0800

    1807 devops day4 12am

--- nsd1807/devops/day04/smail2.py
+++ nsd1807/devops/day04/smail2.py
@@ -9,16 +9,16 @@ def send_mail(text, subject, sender, receivers, server, user, passwd, port=25):
     message['To'] = Header(receivers[0], 'utf8')
     message['Subject'] = Header(subject, 'utf8')
     smtp = SMTP()
-    # smtp.starttls()   # 如果使用证书，打开此注释
     smtp.connect(server, port)
+    # smtp.starttls()   # 如果使用证书，打开此注释
     smtp.login(user, passwd)
-    smtp.sendmail(sender, receivers, message.as_string())
+    smtp.sendmail(sender, receivers, message.as_bytes())
 
 if __name__ == '__main__':
     text = 'python邮件测试\r\n'
     subject = 'smtp test'
     sender = 'zhangzhigang79@126.com'
     passwd = getpass.getpass()
-    server = 'smtp.126.com'
-    receivers = ['zhangzhigang79@126.com']
+    server = 'mail.tedu.cn'
+    receivers = ['zhangzhigang79@126.com', 'zhangzhigang79@qq.com']
     send_mail(text, subject, sender, receivers, server, sender, passwd)
