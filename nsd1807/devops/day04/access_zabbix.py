f8dcb0d921a54fd0fe9c89a1bcee4b670af02441
MrZhangzhg <zhangzg@tedu.cn>
2018-12-22 17:48:27 +0800

    1807 devops day4 18pm

--- nsd1807/devops/day04/access_zabbix.py
+++ nsd1807/devops/day04/access_zabbix.py
@@ -25,22 +25,79 @@ headers = {'Content-Type': 'application/json-rpc'}
 # e00409e83289056318fdf828125931f1
 ################################
 # 获取所有的主机组
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",  # 详细信息
+#         # "filter": {
+#         #     "name": [
+#         #         "Zabbix servers",
+#         #         "Linux servers"
+#         #     ]
+#         # }
+#     },
+#     "auth": "e00409e83289056318fdf828125931f1",
+#     "id": 1
+# }
+# 2: Linux servers
+################################
+# 获取模板信息
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "e00409e83289056318fdf828125931f1",
+#     "id": 1
+# }
+# 'templateid': '10001'
+################################
+# 创建一台主机，进行监控
 data = {
     "jsonrpc": "2.0",
-    "method": "hostgroup.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",  # 详细信息
-        # "filter": {
-        #     "name": [
-        #         "Zabbix servers",
-        #         "Linux servers"
-        #     ]
-        # }
+        "host": "Web Server1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.4.3",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,  # 主机资产记录
+        "inventory": {
+            "macaddress_a": "00000c291234",
+            "macaddress_b": "56768"
+        }
     },
     "auth": "e00409e83289056318fdf828125931f1",
     "id": 1
 }
-# 2: Linux servers
+
+
+
 
 
 r = requests.post(url, headers=headers, data=json.dumps(data))
