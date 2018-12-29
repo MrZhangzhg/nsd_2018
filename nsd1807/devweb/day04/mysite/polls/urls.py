797cc1b980a792048a12ea36eb1811b2e99e1277
MrZhangzhg <zhangzg@tedu.cn>
2018-12-29 15:41:17 +0800

    1807 devweb day1day4 15:40

--- nsd1807/devweb/day04/mysite/polls/urls.py
+++ nsd1807/devweb/day04/mysite/polls/urls.py
@@ -5,5 +5,6 @@ from . import views    # 使用相对导入的方式，在当前包中导入view
 urlpatterns = [
     # 首页用views.index函数处理，为这个URL起个名字叫index
     url(r'^$', views.index, name='index'),
-    url(r'^\d+/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/result/$', views.result, name='result'),
 ]
--- nsd1807/devweb/day04/mysite/polls/views.py
+++ nsd1807/devweb/day04/mysite/polls/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def index(request):    # 用户访问web发来的请求将作为参数request的值
     return render(request, 'index.html')
 
-def detail(request):
-    return render(request, 'detail.html')
+def detail(request, question_id):
+    return render(request, 'detail.html', {'question_id': question_id})
+
+def result(request, question_id):
+    return render(request, 'result.html', {'question_id': question_id})
