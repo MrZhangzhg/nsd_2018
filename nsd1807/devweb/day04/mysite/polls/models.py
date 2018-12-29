5df52104f8ce2bf07626e300de9ef264cc3ed44e
MrZhangzhg <zhangzg@tedu.cn>
2018-12-29 15:59:07 +0800

    1807 devweb day1day4 16:00

--- nsd1807/devweb/day04/mysite/polls/models.py
+++ nsd1807/devweb/day04/mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+    q = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s: %s" % (self.choice_text, self.q)
