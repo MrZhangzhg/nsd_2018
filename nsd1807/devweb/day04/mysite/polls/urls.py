67c4c9d9068b1a18f2c5988ebae158850378a128
MrZhangzhg <zhangzg@tedu.cn>
2018-12-29 14:31:17 +0800

    1807 devweb day1day4 14:30

--- nsd1807/devweb/day04/mysite/polls/urls.py
+++ nsd1807/devweb/day04/mysite/polls/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
+from . import views    # 使用相对导入的方式，在当前包中导入views模块
 
+# 在http://x.x.x.x/polls/后面开始匹配
 urlpatterns = [
+    # 首页用views.index函数处理，为这个URL起个名字叫index
+    url(r'^$', views.index, name='index'),
 ]
--- nsd1807/devweb/day04/mysite/polls/views.py
+++ nsd1807/devweb/day04/mysite/polls/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):    # 用户访问web发来的请求将作为参数request的值
+    return render(request, 'index.html')
