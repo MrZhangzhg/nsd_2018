7fe67e82fb97961c1c3d79824afd182b24e35803
MrZhangzhg <zhangzg@tedu.cn>
2018-12-29 17:06:57 +0800

    1807 devweb day1day4 17:00

--- nsd1807/devweb/day04/mysite/polls/models.py
+++ nsd1807/devweb/day04/mysite/polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
 class Choice(models.Model):
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
-    q = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "%s: %s" % (self.choice_text, self.q)
+        return "%s: %s" % (self.question, self.choice_text)
