af790f6e5faef7d4fab96e0577cb79bcaaa157c0
MrZhangzhg <zhangzg@tedu.cn>
2019-01-02 17:04:31 +0800

    1807 devweb day1day5 17:00

--- nsd1807/devweb/day04/mysite/polls/views.py
+++ nsd1807/devweb/day04/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
 
 def index(request):    # 用户访问web发来的请求将作为参数request的值
@@ -13,5 +13,10 @@ def result(request, question_id):
     return render(request, 'result.html', {'question_id': question_id})
 
 def vote(request, question_id):
-    pass
+    question = Question.objects.get(id=question_id)
+    choice_id = request.POST.get('choice_id')
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+    return redirect('result', question_id=question_id)
 
