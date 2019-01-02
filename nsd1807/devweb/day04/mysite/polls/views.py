93be4aa41a64b660389451588c40672f99e4de92
MrZhangzhg <zhangzg@tedu.cn>
2019-01-02 15:39:13 +0800

    1807 devweb day1day5 15:40

--- nsd1807/devweb/day04/mysite/polls/views.py
+++ nsd1807/devweb/day04/mysite/polls/views.py
@@ -6,7 +6,8 @@ def index(request):    # 用户访问web发来的请求将作为参数request的
     return render(request, 'index.html', {'questions': questions})
 
 def detail(request, question_id):
-    return render(request, 'detail.html', {'question_id': question_id})
+    question = Question.objects.get(id=question_id)
+    return render(request, 'detail.html', {'question': question})
 
 def result(request, question_id):
     return render(request, 'result.html', {'question_id': question_id})
