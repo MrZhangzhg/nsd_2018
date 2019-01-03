ecc0051f77d4aff338b2536a44bc61bc32f2ab34
MrZhangzhg <zhangzg@tedu.cn>
2019-01-03 16:56:45 +0800

    1807 devweb day6 17:00

--- nsd1807/devweb/day06/myblog/blog/models.py
+++ nsd1807/devweb/day06/myblog/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
 
     def __str__(self):
