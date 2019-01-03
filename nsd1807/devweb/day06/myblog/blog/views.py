176a3057920f69a60770818c272bbbbc53c07da0
MrZhangzhg <zhangzg@tedu.cn>
2019-01-03 17:24:55 +0800

    1807 devweb day6 17:30

--- nsd1807/devweb/day06/myblog/blog/views.py
+++ nsd1807/devweb/day06/myblog/blog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, HttpResponse
 from .models import Blog
 
 def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Blog.objects.get_or_create(title=title, text=content)
+
     articles = Blog.objects.order_by('-pub_date')
     return render(request, 'index.html', {'articles': articles})
 
