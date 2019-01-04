c6d6e7cc222d4489bf92ba3d62249ed6b947aae7
MrZhangzhg <zhangzg@tedu.cn>
2019-01-04 17:49:23 +0800

    1807 devweb day7 18:00

--- nsd1807/devweb/ansible_project/myansible/webansi/urls.py
+++ nsd1807/devweb/ansible_project/myansible/webansi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.mainpage, name='mainpage'),
+    url(r'^addhosts/$', views.addhosts, name='addhosts'),
 ]
--- nsd1807/devweb/ansible_project/myansible/webansi/views.py
+++ nsd1807/devweb/ansible_project/myansible/webansi/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import HostGroup
 
 def index(request):
     return render(request, 'index.html')
 
 def mainpage(request):
     return render(request, 'mainpage.html')
+
+def addhosts(request):
+    groups = HostGroup.objects.all()
+    return render(request, 'addhosts.html', {'groups': groups})
