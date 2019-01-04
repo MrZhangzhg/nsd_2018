cfa3053ac0bee89d19e008f97f426a6cd0db868a
MrZhangzhg <zhangzg@tedu.cn>
2019-01-04 11:36:00 +0800

    1807 devweb day7 11:40

--- nsd1807/devweb/ansible_project/myansible/webansi/views.py
+++ nsd1807/devweb/ansible_project/myansible/webansi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>Success</h1>')
+    return render(request, 'index.html')
