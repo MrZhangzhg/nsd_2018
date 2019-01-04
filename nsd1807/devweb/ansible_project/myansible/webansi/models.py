319ee96507bd437bf51c6ca1e4e46091dad47bad
MrZhangzhg <zhangzg@tedu.cn>
2019-01-04 10:33:51 +0800

    1807 devweb day7 10:30

--- nsd1807/devweb/ansible_project/myansible/webansi/models.py
+++ nsd1807/devweb/ansible_project/myansible/webansi/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class HostGroup(models.Model):
+    groupname = models.CharField(max_length=50, unique=True, null=False)
+
+    def __str__(self):
+        return self.groupname
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=100, unique=True, null=False)
+    ipaddr = models.CharField(max_length=15, null=False)
+    group = models.ForeignKey(HostGroup)
+
+    def __str__(self):
+        return '%s => %s' % (self.hostname, self.group)
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=50, unique=True, null=False)
+
+    def __str__(self):
+        return self.module_name
+
+class Argument(models.Model):
+    argument_text = models.CharField(max_length=100, unique=True, null=False)
+    module = models.ForeignKey(Module)
+
+    def __str__(self):
+        return "%s => %s" % (self.module, self.argument_text)
