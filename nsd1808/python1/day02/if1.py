fb1693ec44667d55b9a437d512fc6fc0bdc77907
MrZhangzhg <zhangzg@tedu.cn>
2019-01-12 11:59:57 +0800

    1808 py1 day2 12am

---
+++ nsd1808/python1/day02/if1.py
@@ -0,0 +1,21 @@
+py_str = 'python'
+
+if 'th' in py_str:
+    print('yes')
+
+if 'to' in py_str:
+    print("'to' in str")
+else:
+    print("'to' not in str")
+
+if -0.0:    # False
+    print('0 not ok')
+
+if ' ':     # True
+    print('str ok')
+
+if []:   # False
+    print('list not ok')
+
+if None:   # False
+    print('none not ok')
