67083191fb0450f419a7dce10d1d7d1f53ae43ae
MrZhangzhg <zhangzg@tedu.cn>
2019-01-14 17:42:39 +0800

    1808 py1 day3 18pm

--- nsd1808/python1/day03/star.py
+++ nsd1808/python1/day03/star.py
@@ -3,5 +3,6 @@ hi = 'Hello World'
 def pstar(n=30):
     print('*' * n)
 
-pstar()   # 调用函数时，没有指定参数，就使用函数默认的参数值
-pstar(50)
+if __name__ == '__main__':
+    pstar()   # 调用函数时，没有指定参数，就使用函数默认的参数值
+    pstar(50)
