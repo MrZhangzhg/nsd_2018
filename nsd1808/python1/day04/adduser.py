ea8b1ecf1ec850e97074e6a395b1c3addb2584fb
MrZhangzhg <zhangzg@tedu.cn>
2019-01-15 16:32:52 +0800

    1808 py1 day4 16:30pm

---
+++ nsd1808/python1/day04/adduser.py
@@ -0,0 +1,19 @@
+import sys
+import randpass2
+import subprocess
+
+def adduser(username, password, userfile):
+    user_info = """username: %s
+password: %s
+""" % (username, password)   # 写入文件的用户信息
+    subprocess.call('useradd %s' % username, shell=True)
+    subprocess.call(
+        'echo %s | passwd --stdin %s' % (password, username),
+        shell=True
+    )
+    with open(userfile, 'a') as fobj:
+        fobj.write(user_info)   # 把用户信息写入文件
+
+if __name__ == '__main__':
+    password = randpass2.gen_pass()
+    adduser(sys.argv[1], password, '/tmp/user.info')
