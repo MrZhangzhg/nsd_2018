33a9dd05add60ec0c68da9e8a1a2f7768ed3747e
MrZhangzhg <zhangzg@tedu.cn>
2019-01-23 11:00:13 +0800

    1808 devops day2 11am

--- nsd1808/devops/day02/mydcl.py
+++ nsd1808/devops/day02/mydcl.py
@@ -15,6 +15,23 @@ dep_id INT, dep_name VARCHAR(20) NOT NULL UNIQUE, PRIMARY KEY(dep_id))
 """
 cursor.execute(create_dep)
 
+create_employess = """CREATE TABLE employees(
+emp_id INT, emp_name VARCHAR(20) NOT NULL, gender VARCHAR(6),
+birth_date DATE, email VARCHAR(50), dep_id INT,
+PRIMARY KEY(emp_id),
+FOREIGN KEY (dep_id) REFERENCES departments(dep_id)
+)
+"""
+cursor.execute(create_employess)
+
+create_salary = """CREATE TABLE salary(
+auto_id INT, date DATE, emp_id INT, basic INT, awards INT,
+PRIMARY KEY(auto_id),
+FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+)
+"""
+cursor.execute(create_salary)
+
 cursor.close()
 conn.close()
 
