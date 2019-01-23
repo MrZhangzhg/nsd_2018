c59a483ae671e93cbb8ffeb3e9f62e8d4ef1b42b
MrZhangzhg <zhangzg@tedu.cn>
2019-01-23 11:52:00 +0800

    1808 devops day2 12am

--- nsd1808/devops/day02/crud.py
+++ nsd1808/devops/day02/crud.py
@@ -11,10 +11,40 @@ conn = pymysql.connect(
 cursor = conn.cursor()
 
 insert1 = 'INSERT INTO departments VALUES(%s, %s)'
-cursor.execute(insert1, (1, 'HR'))
-deps = [(2, '运维'), (3, '开发'), (4, '测试')]
-cursor.executemany(insert1, deps)
+# cursor.execute(insert1, (1, 'HR'))
+# deps = [(2, '运维'), (3, '开发'), (4, '测试')]
+# cursor.executemany(insert1, deps)
+# deps2 = [(5, '行政'), (6, '财务'), (7, '市场')]
+# cursor.executemany(insert1, deps2)
+# conn.commit()
+
+# query1 = 'SELECT * FROM departments ORDER BY dep_id'
+# cursor.execute(query1)
+# result1 = cursor.fetchone()
+# print(result1)
+# print('*' * 30)
+# result2 = cursor.fetchmany(3)
+# print(result2)
+# print('*' * 30)
+# result3 = cursor.fetchall()
+# print(result3)
+#################################
+# query1 = 'SELECT * FROM departments ORDER BY dep_id'
+# cursor.execute(query1)
+# cursor.scroll(1, mode='absolute')
+# print(cursor.fetchone())
+# cursor.scroll(2, mode='relative')
+# print(cursor.fetchone())
+#################################
+# update1 = 'UPDATE departments SET dep_name=%s WHERE dep_name=%s'
+# cursor.execute(update1, ('人事部', 'HR'))
+# conn.commit()
+#################################
+delete1 = 'DELETE FROM departments WHERE dep_id=%s'
+cursor.execute(delete1, (7,))
 conn.commit()
+
+
 
 cursor.close()
 conn.close()
