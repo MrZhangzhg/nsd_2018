a16bddec21150088535c5670a21e1b28db01b468
MrZhangzhg <zhangzg@tedu.cn>
2019-01-25 11:51:27 +0800

    1808 devops day3 12am

--- nsd1808/devops/day03/get_web.py
+++ nsd1808/devops/day03/get_web.py
@@ -1,4 +1,6 @@
 import re
+import os
+import urllib.error
 from urllib import request
 
 def download(url, fname):
@@ -11,11 +13,27 @@ def download(url, fname):
             fobj.write(data)
 
 def get_url(fname, patt, encoding=None):
-    
+    url_list = []
+    cpatt = re.compile(patt)
+    with open(fname, encoding=encoding) as fobj:
+        for line in fobj:
+            for m in cpatt.finditer(line):    # [匹配对象，匹配对象]
+                url_list.append(m.group())
+    return url_list
 
 if __name__ == '__main__':
     net_url = 'http://www.163.com/'
     fname = '/tmp/163.html'
-    url_patt = '(http|https)://[\w.-/]+\.(png|jpg|jpeg|gif)'
+    url_patt = '(http|https)://[-\w./]+\.(png|jpg|jpeg|gif)'
     download(net_url, fname)
     url_list = get_url(fname, url_patt, encoding='gbk')
+    folder = '/tmp/wangyi/'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    for url in url_list:
+        fname = url.split('/')[-1]
+        fname = os.path.join(folder, fname)
+        try:
+            download(url, fname)
+        except urllib.error.HTTPError:
+            print(url)
