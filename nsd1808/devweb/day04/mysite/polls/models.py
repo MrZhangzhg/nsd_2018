c703f2c65b7cc7f63b65f96adc06fde9112ee064
MrZhangzhg <zhangzg@tedu.cn>
2019-02-11 16:51:02 +0800

    1808 devweb day04 17:00

--- nsd1808/devweb/day04/mysite/polls/models.py
+++ nsd1808/devweb/day04/mysite/polls/models.py
@@ -6,3 +6,13 @@ class Question(models.Model):
 
     def __str__(self):
         return '<问题：%s>' % self.question
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    # q = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "<%s=>%s>" % (Question, self.choice_text)
+
