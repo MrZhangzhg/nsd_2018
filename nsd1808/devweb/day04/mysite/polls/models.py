d1895cf7d2b0a29d71a08b79029ba337eccd95e4
MrZhangzhg <zhangzg@tedu.cn>
2019-02-11 16:06:33 +0800

    1808 devweb day04 16:10

--- nsd1808/devweb/day04/mysite/polls/models.py
+++ nsd1808/devweb/day04/mysite/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=200, null=False, unique=True)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return '<问题：%s>' % self.question
