6b610efa2ca9f8c3204a220e609907585b50527b
MrZhangzhg <zhangzg@tedu.cn>
2019-02-12 14:26:00 +0800

    1808 devweb day5 14:30

--- nsd1808/devweb/day04/mysite/polls/urls.py
+++ nsd1808/devweb/day04/mysite/polls/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
 ]
 
--- nsd1808/devweb/day04/mysite/polls/views.py
+++ nsd1808/devweb/day04/mysite/polls/views.py
@@ -6,3 +6,7 @@ def index(request):
     # return HttpResponse('<h1>投票首页</h1>')
     questions = Question.objects.order_by('-pub_date')
     return render(request, 'index.html', {'questions': questions})
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    return render(request, 'detail.html', {'question': question})
