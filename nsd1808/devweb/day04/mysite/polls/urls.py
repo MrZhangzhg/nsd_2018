fa5cc3ff9a46e78cacbf46b7b5828ab59a48911f
MrZhangzhg <zhangzg@tedu.cn>
2019-02-12 14:49:35 +0800

    1808 devweb day5 14:50

--- nsd1808/devweb/day04/mysite/polls/urls.py
+++ nsd1808/devweb/day04/mysite/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/result/$', views.result, name='detail'),
 ]
 
--- nsd1808/devweb/day04/mysite/polls/views.py
+++ nsd1808/devweb/day04/mysite/polls/views.py
@@ -10,3 +10,7 @@ def index(request):
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     return render(request, 'detail.html', {'question': question})
+
+def result(request, question_id):
+    question = Question.objects.get(id=question_id)
+    return render(request, 'result.html', {'question': question})
