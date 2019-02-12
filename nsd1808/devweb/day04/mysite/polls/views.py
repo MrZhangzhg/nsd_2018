0f4dc688c55659ad9fc7fb554b5280e9e3398067
MrZhangzhg <zhangzg@tedu.cn>
2019-02-12 11:55:51 +0800

    1808 devweb day5 12:00

--- nsd1808/devweb/day04/mysite/polls/views.py
+++ nsd1808/devweb/day04/mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .models import Question
 
 def index(request):
     # 形参request名称随便定义，但是必须提供，用户的请求将作为实参传入
     # return HttpResponse('<h1>投票首页</h1>')
-    return render(request, 'index.html')
+    questions = Question.objects.order_by('-pub_date')
+    return render(request, 'index.html', {'questions': questions})
