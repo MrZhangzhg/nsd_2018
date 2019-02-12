1578a70ad557a767821b2937a117f2264fa3644b
MrZhangzhg <zhangzg@tedu.cn>
2019-02-12 09:52:49 +0800

    1808 devweb day5 10:00

--- nsd1808/devweb/day04/mysite/polls/models.py
+++ nsd1808/devweb/day04/mysite/polls/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Question(models.Model):
-    question = models.CharField(max_length=200, null=False, unique=True)
+    question_text = models.CharField(max_length=200, null=False, unique=True)
     pub_date = models.DateField()
 
     def __str__(self):
-        return '<问题：%s>' % self.question
+        return '<问题：%s>' % self.question_text
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
