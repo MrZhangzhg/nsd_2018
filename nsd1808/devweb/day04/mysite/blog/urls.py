9258002815a0d03e8cf39c37c49ec7ca9f63373d
MrZhangzhg <zhangzg@tedu.cn>
2019-02-13 18:00:23 +0800

    1808 devweb day6 18:00

--- nsd1808/devweb/day04/mysite/blog/urls.py
+++ nsd1808/devweb/day04/mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='blog_index'),
+    url(r'^(?P<article_id>\d+)/$', views.show_article, name='show_article'),
 ]
--- nsd1808/devweb/day04/mysite/blog/views.py
+++ nsd1808/devweb/day04/mysite/blog/views.py
@@ -4,3 +4,7 @@ from .models import Article
 def index(request):
     articles = Article.objects.order_by('-pub_date')
     return render(request, 'blog_index.html', {'articles': articles})
+
+def show_article(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'show_article.html', {'article': article})
