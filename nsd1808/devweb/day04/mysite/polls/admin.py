1fb1d73f86136a48d4f15bc6c0b66b435ddfcbf0
MrZhangzhg <zhangzg@tedu.cn>
2019-02-13 14:56:00 +0800

    1808 devweb day6 14:56

--- nsd1808/devweb/day04/mysite/polls/admin.py
+++ nsd1808/devweb/day04/mysite/polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
