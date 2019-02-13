4d543687002f67799572d95ebe8e52f50fbccfd9
MrZhangzhg <zhangzg@tedu.cn>
2019-02-13 15:43:45 +0800

    1808 devweb day6 15:40

--- nsd1808/devweb/day04/mysite/polls/admin.py
+++ nsd1808/devweb/day04/mysite/polls/admin.py
@@ -3,6 +3,13 @@ from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
+    date_hierarchy = 'pub_date'
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+    ordering = ('-pub_date',)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    raw_id_fields = ('question',)
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
