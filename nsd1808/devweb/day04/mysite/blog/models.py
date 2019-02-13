985c651bce88020ad56cf62638e425e022d76efc
MrZhangzhg <zhangzg@tedu.cn>
2019-02-13 17:01:24 +0800

    1808 devweb day6 17:00

--- nsd1808/devweb/day04/mysite/blog/models.py
+++ nsd1808/devweb/day04/mysite/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    pub_date = models.DateTimeField(auto_now_add=True)  # 自动使用发布文章时的时间
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
