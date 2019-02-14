b91e9c920cf55a16a0a49459ad873986f0d28b56
MrZhangzhg <zhangzg@tedu.cn>
2019-02-14 14:13:32 +0800

    1808 devweb day7 14:10

--- nsd1808/devweb/ansible_project/myansible/ansicfg/dhosts.py
+++ nsd1808/devweb/ansible_project/myansible/ansicfg/dhosts.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker
+import json
 
 engine = create_engine(
     'sqlite:////var/ftp/nsd_2018/nsd1808/devweb/ansible_project/myansible/db.sqlite3',
@@ -28,4 +29,23 @@ class Host(Base):
 if __name__ == '__main__':
     session = Session()
     query = session.query(HostGroup.group_name, Host.ipaddr).join(Host)
-    print(query.all())
+    # print(query.all())
+    # for group, ip in query:    # 取出查询结果的组和IP地址
+    #     print(group, ip)
+    # result = {}
+    # for group, ip in query:   # 构建result = {'g1': {}, 'g2': {}}
+    #     if group not in result:
+    #         result[group] = {}
+    # print(result)
+    # for group, ip in query:
+    #     if group not in result:
+    #         result[group] = {}
+    #         result[group]['hosts'] = []  构建result = {'g1': {'hosts': []}, 'g2': {'hosts': []}}
+    # print(result)
+    result = {}
+    for group, ip in query:
+        if group not in result:
+            result[group] = {}
+            result[group]['hosts'] = []
+        result[group]['hosts'].append(ip)  # 向列表中追加主机IP地址
+    print(json.dumps(result))
