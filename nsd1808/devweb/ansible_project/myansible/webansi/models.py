8cf1713314be8c93e55dd12b07424505589e8c90
MrZhangzhg <zhangzg@tedu.cn>
2019-02-14 10:17:52 +0800

    1808 devweb day7 10:20

--- nsd1808/devweb/ansible_project/myansible/webansi/models.py
+++ nsd1808/devweb/ansible_project/myansible/webansi/models.py
@@ -13,3 +13,16 @@ class Host(models.Model):
 
     def __str__(self):
         return "%s => %s" % (self.hostname, self.group)
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=50, unique=True, null=False)
+
+    def __str__(self):
+        return self.module_name
+
+class Argument(models.Model):
+    argument_text = models.CharField(max_length=100, null=False)
+    module = models.ForeignKey(Module)
+
+    def __str__(self):
+        return "%s => %s" % (self.module, self.argument_text)
