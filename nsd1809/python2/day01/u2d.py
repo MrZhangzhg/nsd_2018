191c9b8293e52159325666f1e412f185a81c38a8
MrZhangzhg <zhangzg@tedu.cn>
2019-02-16 16:19:56 +0800

    1809 python2 day1 16:21

--- nsd1809/python2/day01/u2d.py
+++ nsd1809/python2/day01/u2d.py
@@ -1,12 +1,13 @@
 import sys
 
 def unix2dos(fname):
-    dst_fname = fname + '.txt'
+    dst_fname = fname + '.txt'   # 新文件名是原文件尾部加上.txt后缀
     with open(fname) as src_fobj:
         with open(dst_fname, 'w') as dst_fobj:
             for line in src_fobj:
+                # 把原文件结尾的\n\r组合全部消除，再加上\r\n
                 line = line.rstrip('\n\r') + '\r\n'
-                dst_fobj.write(line)
+                dst_fobj.write(line)  # 写入新文件
 
 if __name__ == '__main__':
-    unix2dos(sys.argv[1])
+    unix2dos(sys.argv[1])   # 文件名使用位置参数接收
