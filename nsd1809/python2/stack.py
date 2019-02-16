ba638f19bb6c0732c68f2b8e51ce99d4a89c40d5
MrZhangzhg <zhangzg@tedu.cn>
2019-02-16 11:32:49 +0800

    1809 python2 day1 11:30

--- nsd1809/python2/stack.py
+++ nsd1809/python2/stack.py
@@ -3,15 +3,33 @@
 栈：是一个后进先出的结构
 """
 def push_it():
-
+    print('push')
 
 def pop_it():
-
+    print('pop')
 
 def view_it():
-
+    print('view')
 
 def show_menu():
+    prompt = """(0) push it
+(1) pop it
+(2) view it
+(3) quit
+Please input your choice(0/1/2/3): """
+    while True:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print('Invalid choice. Try again.')
+            continue
+        if choice == '3':
+            break
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        else:
+            view_it()
 
 
 if __name__ == '__main__':
