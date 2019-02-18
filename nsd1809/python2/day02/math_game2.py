d751c93e9a356c2d3d8c4ab5c9f1482efa27a653
MrZhangzhg <zhangzg@tedu.cn>
2019-02-18 16:10:46 +0800

    1809 python2 day2 16:10

--- nsd1809/python2/day02/math_game2.py
+++ nsd1809/python2/day02/math_game2.py
@@ -13,17 +13,34 @@ def exam():
     op = random.choice('+-')
     result = cmds[op](*nums)
     prompt = '%s %s %s = ' % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('Very good')
-    else:
+    counter = 0
+
+    while counter <= 2:
+        try:
+            answer = int(input(prompt))
+        except:     # 空except语句，可以捕获所有异常，但是不推荐
+            print() # 打印回车
+            continue
+
+        if answer == result:
+            print('Very good')
+            break
         print('Wrong answer')
+        counter += 1
+    else:   # 循环正常结束，没有被break才执行的语句
+        print('%s%s' % (prompt, result))
 
 def main():
     while True:
         exam()
         # 将用户输入的内容去除两端空白字符后，取出第一个字符
-        yn = input('Contine(y/n)? ').strip()[0]
+        try:
+            yn = input('Contine(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'    # 如果按了ctrl+c/d，算用户回答了n
+
         if yn in 'nN':  # 只有第一个字符是n或N才结束
             print('\nByebye')
             break
