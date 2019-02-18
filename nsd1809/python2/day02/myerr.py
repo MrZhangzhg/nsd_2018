e31d4cd75d045dd0a9cef925040be369b8246b13
MrZhangzhg <zhangzg@tedu.cn>
2019-02-18 11:51:47 +0800

    1809 python2 day2 11:50

--- nsd1809/python2/day02/myerr.py
+++ nsd1809/python2/day02/myerr.py
@@ -1,11 +1,14 @@
 try:
     num = int(input('数字：'))
     result = 100 / num
-    print(result)
 except (ValueError, ZeroDivisionError):
     print('无效的输入')
 except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
     exit()
+else:       # 不发生异常才执行
+    print(result)
+finally:    # 不管异常是否发生，都会执行
+    print('Done')
 
-print('Done')
+# 常用的组合是try-except和try-finally
