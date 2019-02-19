5f4b82ce289ec5d5a7c39e9945eb7027ec35ed7f
MrZhangzhg <zhangzg@tedu.cn>
2019-02-19 11:56:10 +0800

    1809 python2 day3 12:00

---
+++ nsd1809/python2/day03/quick_sort.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def qsort(seq):
+    """
+    将第一个数假设为中间值，比它小的放到小列表，大的放到大列表，再把这三项
+    拼接起来。小列表、大列表仍然要用同样的方法继承排序
+    """
+    if len(seq) < 2:
+        return seq
+
+    middle = seq[0]
+    smaller = []
+    larger = []
+    for item in seq[1:]:
+        if item < middle:
+            smaller.append(item)
+        else:
+            larger.append(item)
+    return qsort(smaller) + [middle] + qsort(larger)
+
+
+if __name__ == '__main__':
+    nums = [randint(1, 100) for i in range(10)]
+    print(nums)
+    print(qsort(nums))
