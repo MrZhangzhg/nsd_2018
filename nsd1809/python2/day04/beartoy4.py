b3ab548693a97baa56e1e3aac7a019dae305382b
MrZhangzhg <zhangzg@tedu.cn>
2019-02-20 14:31:47 +0800

    1809 python2 day4 14:30

--- nsd1809/python2/day04/beartoy4.py
+++ nsd1809/python2/day04/beartoy4.py
@@ -7,10 +7,16 @@ class Bear:
         print("My color is %s, Lalala" % self.color)
 
 class NewBear(Bear):  # NewBear的父类(基类)是Bear
+    def __init__(self, name, color, size):
+        # Bear.__init__(self, color, size)
+        super(NewBear, self).__init__(color, size)  # 推荐写法
+        self.name = name
+
     def run(self):
         print('I can run......')
 
 if __name__ == '__main__':
-    bear1 = NewBear('Brown', 'Middle')
+    bear1 = NewBear('熊大', 'Brown', 'Middle')
     bear1.sing()  # 子类直接拥有父类的方法
     bear1.run()
+    print(bear1.name)
