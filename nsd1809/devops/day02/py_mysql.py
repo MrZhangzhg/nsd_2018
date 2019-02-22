17dd9d3dd3c7de6985a8bebf9e68b0828dafd3aa
MrZhangzhg <zhangzg@tedu.cn>
2019-02-22 15:36:41 +0800

    1809 devops day02 15:40

---
+++ nsd1809/devops/day02/py_mysql.py
@@ -0,0 +1,20 @@
+import pymysql
+
+conn = pymysql.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    passwd='tedu.cn',
+    db='nsd1809',
+    charset='utf8'
+)
+cursor = conn.cursor()  # 创建游标，类似于文件对象，用它来执行SQL语句
+
+create_employees = """
+
+"""
+cursor.execute(create_employees)
+
+conn.commit()
+cursor.close()
+conn.close()
