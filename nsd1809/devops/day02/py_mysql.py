59b1ac62bce3819fa93383cb5d03dbeb1c24b909
MrZhangzhg <zhangzg@tedu.cn>
2019-02-22 16:08:54 +0800

    1809 devops day02 16:10

--- nsd1809/devops/day02/py_mysql.py
+++ nsd1809/devops/day02/py_mysql.py
@@ -10,17 +10,26 @@ conn = pymysql.connect(
 )
 cursor = conn.cursor()  # 创建游标，类似于文件对象，用它来执行SQL语句
 
-create_departments = """CREATE TABLE departments(
-dep_id INT, dep_name VARCHAR(20) NOT NULL UNIQUE, 
-PRIMARY KEY(dep_id)
-)"""
-cursor.execute(create_departments)
+# create_departments = """CREATE TABLE departments(
+# dep_id INT, dep_name VARCHAR(20) NOT NULL UNIQUE,
+# PRIMARY KEY(dep_id)
+# )"""
+# cursor.execute(create_departments)
 
 # create_employees = """CREATE TABLE employees(
 # emp_id INT, emp_name VARCHAR(20), gender VARCHAR(6),
-# birth_date DATE, email VARCHAR(50), dep_id INT
+# birth_date DATE, email VARCHAR(50), dep_id INT,
+# PRIMARY KEY(emp_id),
+# FOREIGN KEY(dep_id) REFERENCES departments(dep_id)
 # )"""
 # cursor.execute(create_employees)
+
+create_salary = """CREATE TABLE salary(
+auto_id INT, emp_id INT, date DATE, basic INT, awards INT,
+PRIMARY KEY(auto_id),
+FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+)"""
+cursor.execute(create_salary)
 
 conn.commit()
 cursor.close()
