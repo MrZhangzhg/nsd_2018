c08bf09cddc85d986d2feda9c2d85068ba056c17
MrZhangzhg <zhangzg@tedu.cn>
2019-02-22 15:51:12 +0800

    1809 devops day02 15:50

--- nsd1809/devops/day02/py_mysql.py
+++ nsd1809/devops/day02/py_mysql.py
@@ -10,10 +10,17 @@ conn = pymysql.connect(
 )
 cursor = conn.cursor()  # 创建游标，类似于文件对象，用它来执行SQL语句
 
-create_employees = """
+create_departments = """CREATE TABLE departments(
+dep_id INT, dep_name VARCHAR(20) NOT NULL UNIQUE, 
+PRIMARY KEY(dep_id)
+)"""
+cursor.execute(create_departments)
 
-"""
-cursor.execute(create_employees)
+# create_employees = """CREATE TABLE employees(
+# emp_id INT, emp_name VARCHAR(20), gender VARCHAR(6),
+# birth_date DATE, email VARCHAR(50), dep_id INT
+# )"""
+# cursor.execute(create_employees)
 
 conn.commit()
 cursor.close()
