9fdc5b08e196d7b04add7e595ade8ed95bdebf1a
MrZhangzhg <zhangzg@tedu.cn>
2019-02-26 16:16:30 +0800

    1809 devops day0 16:200

---
+++ nsd1809/devops/day04/send_email.py
@@ -0,0 +1,24 @@
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+import getpass
+
+def send_email(text, sender, receivers, subject, host, user, passwd):
+    message = MIMEText(text, 'plain', 'utf8')
+    message['From'] = Header(sender, 'utf8')
+    message['To'] = Header(receivers[0], 'utf8')
+    message['Subject'] = Header(subject, 'utf8')
+
+    smtp = smtplib.SMTP(host)
+    smtp.login(user, passwd)
+    smtp.sendmail(sender, receivers, message.as_bytes())
+
+if __name__ == '__main__':
+    text = '这是一封python邮件测试\r\n'
+    sender = 'zhangzhigang79@126.com'
+    receivers = ['zhangzhigang79@126.com']
+    subject = '邮件测试'
+    host = 'smtp.126.com'
+    user = 'zhangzhigang79@126.com'
+    passwd = getpass.getpass()
+    send_email(text, sender, receivers, subject, host, user, passwd)
