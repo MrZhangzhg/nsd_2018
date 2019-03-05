d7d3068cd9b629fd0b9cdbdee0bf6b1e0111fd93
MrZhangzhg <zhangzg@tedu.cn>
2019-03-05 17:34:30 +0800

    1809 devweb day03 17:30

--- nsd1809/devweb/day04/mysite/polls/views.py
+++ nsd1809/devweb/day04/mysite/polls/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def detail(request, question_id):
+    return render(request, 'detail.html', {'question_id': question_id})
