42a9f446762150dfc8903cc05eaced17372499b4
MrZhangzhg <zhangzg@tedu.cn>
2019-03-06 11:01:00 +0800

    1809 devweb day04 11:00

--- nsd1809/devweb/day04/mysite/polls/models.py
+++ nsd1809/devweb/day04/mysite/polls/models.py
@@ -13,4 +13,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
 
     def __str__(self):
-        return "%s=>%s: %s" % (self.q, self.choice_text, self.votes)
+        return "%s=>%s: %s" % (self.question, self.choice_text, self.votes)
