7b05fb62d282520683e2d027f2369cd0b793f6e6
MrZhangzhg <zhangzg@tedu.cn>
2019-03-06 15:46:36 +0800

    1809 devweb day04 15:50

--- nsd1809/devweb/day04/mysite/polls/views.py
+++ nsd1809/devweb/day04/mysite/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return render(request, 'index.html')
+    questions = Question.objects.order_by('-pub_date')
+    return render(request, 'index.html', {'questions': questions})
 
 def detail(request, question_id):
     return render(request, 'detail.html', {'question_id': question_id})
