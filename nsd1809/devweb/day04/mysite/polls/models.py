47d68afc83e8d3832f631e8718bf6e9c8a259752
MrZhangzhg <zhangzg@tedu.cn>
2019-03-06 10:26:17 +0800

    1809 devweb day04 10:30

--- nsd1809/devweb/day04/mysite/polls/models.py
+++ nsd1809/devweb/day04/mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, unique=True, null=False)
+    pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField()
+    q = models.ForeignKey(Question)
+
+    def __str__(self):
+        return "%s=>%s: %s" % (self.q, self.choice_text, self.votes)
