a45bf2a5797736259c1cc1b1570b4e216b45d442
MrZhangzhg <zhangzg@tedu.cn>
2019-03-13 16:45:54 +0800

    1810 python1 day03 16:50

--- nsd1810/python1/day03/fib_func.py
+++ nsd1810/python1/day03/fib_func.py
@@ -1,12 +1,19 @@
-def gen_fib():
+def gen_fib(num):
     fib = [0, 1]
 
-    num = int(input('数列长度: '))
+    # num = int(input('数列长度: '))
     for i in range(num - 2):
         fib.append(fib[-1] + fib[-2])
 
     return fib
 
-nums = gen_fib()
-nums2 = [i * 2 for i in nums]
-print(nums2)
+# nums = gen_fib()
+# nums2 = [i * 2 for i in nums]
+# print(nums2)
+nlist = [5, 8, 10]
+for n in nlist:
+    result = gen_fib(n)
+    print(result)
+
+l = int(input('数列长度: '))
+print(gen_fib(l))
