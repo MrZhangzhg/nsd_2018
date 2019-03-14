ea66985e76bccf2b79f6aa0761fa12b02089e49c
MrZhangzhg <zhangzg@tedu.cn>
2019-03-14 11:24:15 +0800

    1810 python1 day04 11:20

---
+++ nsd1810/python1/day04/mktxtfile.py
@@ -0,0 +1,19 @@
+"""
+1、发呆。脑补程序运行的样子：是交互的？非交互的？
+交互的：屏幕提示？回答什么？
+2、分析程序有哪些功能，将每个功能编写成函数。再从主程序部分定义函数什么时候调用
+3、编写具体的函数
+"""
+def get_fname():
+
+
+def get_content():
+
+
+def wfile(fname, content):
+
+
+if __name__ == '__main__':
+    fname = get_fname()
+    content = get_content()
+    wfile(fname, content)
