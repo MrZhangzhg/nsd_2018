6cdd12acd48695c46dabc82d4606a2d204862d80
MrZhangzhg <zhangzg@tedu.cn>
2019-03-14 14:17:49 +0800

    1810 python1 day04 14:10

--- nsd1810/python1/day04/day04.py
+++ nsd1810/python1/day04/day04.py
@@ -1,2 +1,11 @@
-import shutil
 
+alist = [10, 30, 20]
+
+for item in enumerate(alist):   # [(0, 10), (1, 30), (2, 20)]
+    print(item)
+
+for item in enumerate(alist):
+    print('%s: %s' % (item[0], item[1]))
+
+for ind, num in enumerate(alist):
+    print('%s: %s' % (ind, num))
