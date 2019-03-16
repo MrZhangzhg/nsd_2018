49411c69c2ea3b2b18c424226b706cf3bd683486
MrZhangzhg <zhangzg@tedu.cn>
2019-03-16 09:25:17 +0800

    1810 python2 day01 9:20

--- nsd1810/python2/day01/stack.py
+++ nsd1810/python2/day01/stack.py
@@ -8,6 +8,30 @@ def view_it():
 
 
 def show_menu():
+    cmds = {'0': push_it, '1': pop_it, '2': view_it}  # 把函数存入字典
+    prompt = """(0) push it
+(1) pop it
+(2) view it
+(3) quit
+Please input your choice(0/1/2/3): """
+    while True:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print('Invalid input. Try again.')
+            continue
+
+        if choice == '3':
+            break
+        cmds[choice]()   # choice = '0' -> cmds['0']  ->push_it
+
+        # if choice == '0':
+        #     push_it()
+        # elif choice == '1':
+        #     pop_it()
+        # elif choice == '2':
+        #     view_it()
+        # else:
+        #     break
 
 
 if __name__ == '__main__':
