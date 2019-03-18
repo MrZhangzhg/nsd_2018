a3ad549a936aaac140bf79f87977aa5481ff6c4a
MrZhangzhg <zhangzg@tedu.cn>
2019-03-18 15:18:16 +0800

    1810 python2 day02 15:20

--- nsd1810/python2/day02/anonymous.py
+++ nsd1810/python2/day02/anonymous.py
@@ -6,20 +6,30 @@
 #     myadd = lambda x, y: x + y
 #     print(myadd(10, 20))
 
+# from random import randint
+#
+# def func1(num):
+#     return num % 2   # 返回值是1即为True，返回值是0即为False
+#
+# if __name__ == '__main__':
+#     alist = [randint(1, 100) for i in range(20)]
+#     print(alist)  # [47, 57, 19, 39, 91, 71, 44]
+#     result = filter(func1, alist)
+#     print(list(result))
+#     # filter将它第2个参数中的每一项，作为第一个参数(函数)的参数传递
+#     # 如果func1的返回值是True就留下来，否则过滤掉
+#     result2 = filter(lambda num: num % 2, alist)
+#     print(list(result2))
+
 from random import randint
 
-def func1(num):
-    return num % 2   # 返回值是1即为True，返回值是0即为False
+def func2(num):
+    return num * 2
 
 if __name__ == '__main__':
-    alist = [randint(1, 100) for i in range(20)]
-    print(alist)  # [47, 57, 19, 39, 91, 71, 44]
-    result = filter(func1, alist)
+    alist = [randint(1, 100) for i in range(10)]
+    print(alist)
+    result = map(func2, alist)   # 将列表中的每一项作为func2的参数，进行加工
     print(list(result))
-    # filter将它第2个参数中的每一项，作为第一个参数(函数)的参数传递
-    # 如果func1的返回值是True就留下来，否则过滤掉
-    result2 = filter(lambda num: num % 2, alist)
+    result2 = map(lambda num: num * 2, alist)
     print(list(result2))
-
-
-
