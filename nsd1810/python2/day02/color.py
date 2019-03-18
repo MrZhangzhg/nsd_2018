e955fd74a68650ea5237778f2c0a950381a39ac7
MrZhangzhg <zhangzg@tedu.cn>
2019-03-18 18:00:29 +0800

    1810 python2 day02 18:01

--- nsd1810/python2/day02/color.py
+++ nsd1810/python2/day02/color.py
@@ -15,3 +15,6 @@ if __name__ == '__main__':
     a = set_color(hello)
     print(a())
     print(greet())
+    # print(set_color(greet)())
+# greet因为有装饰器，所有这里的greet其实是把greet函数作为set_color的参数传进去
+# 返回red，有装饰器的greet相当于是set_color(greet)()
