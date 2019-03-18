40c15072866da1702a65b4a59e11d8f0fba7865c
MrZhangzhg <zhangzg@tedu.cn>
2019-03-18 14:14:33 +0800

    1810 python2 day02 14:10

--- nsd1810/python2/day02/math_game2.py
+++ nsd1810/python2/day02/math_game2.py
@@ -13,16 +13,33 @@ def exam():
     op = choice('+-')
     result = cmds[op](*nums)
     prompt = '%s %s %s = ' % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('Very Good!')
-    else:
+    counter = 0
+
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:
+            print()
+            continue
+
+        if answer == result:
+            print('Very Good!')
+            break
         print('Wrong Answer!')
+        counter += 1
+    else:
+        print('%s%s' % (prompt, result))
 
 def main():
     while True:
         exam()
-        yn = input('Continue(y/n)? ').strip()[0]  # 取出用户输入的第一个非空字符
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]  # 取出用户输入的第一个非空字符
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
         if yn in 'nN':
             print('\nBye-bye')
             break
