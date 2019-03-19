5d090e7e1c7d90a9a02e7855dd649b211b71ba45
MrZhangzhg <zhangzg@tedu.cn>
2019-03-19 09:32:37 +0800

    1810 python2 day03 9:30

--- nsd1810/python2/day03/timeit.py
+++ nsd1810/python2/day03/timeit.py
@@ -1,15 +1,24 @@
 import time
 
-def timeit(func):
-    start = time.time()
-    func()
-    end = time.time()
-    print(end - start)
+def deco(func):
+    def timeit():
+        start = time.time()
+        func()
+        end = time.time()
+        print(end - start)
+    return timeit
 
+@deco
 def func1():
     print('func start')
     time.sleep(3)
     print('func end')
 
+def func2():
+    print('func start')
+    time.sleep(3)
+    print('func end')
+
 if __name__ == '__main__':
-    timeit(func1)
+    func1()
+    deco(func2)()
