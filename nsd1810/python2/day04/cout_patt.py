e896864682d9ea94d7f02ec3696bfb5a393dc779
MrZhangzhg <zhangzg@tedu.cn>
2019-03-20 14:11:14 +0800

    1810 python2 day04 14:10

--- nsd1810/python2/day04/cout_patt.py
+++ nsd1810/python2/day04/cout_patt.py
@@ -1,5 +1,13 @@
 
+def count_pat(fname, patt):
 
 
-
-
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = ''
+    print(count_pat(fname, ip))
+    br = ''
+    print(count_pat(fname, br))
+    fname = '/etc/passwd'
+    shell = ''
+    print(count_pat(fname, shell))
