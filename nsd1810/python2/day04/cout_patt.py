8cd6334959a80b66fa1599bbba8d97632ebb5cc3
MrZhangzhg <zhangzg@tedu.cn>
2019-03-20 14:33:34 +0800

    1810 python2 day04 14:30

--- nsd1810/python2/day04/cout_patt.py
+++ nsd1810/python2/day04/cout_patt.py
@@ -1,13 +1,24 @@
+import re
 
 def count_pat(fname, patt):
+    result = {}
+    cpatt = re.compile(patt)
 
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:
+                key = m.group()
+                result[key] = result.get(key, 0) + 1
+
+    return result
 
 if __name__ == '__main__':
     fname = 'access_log'
-    ip = ''
+    ip = '^(\d+\.){3}\d+'  # 1.11.111.25   1234.56789.1.235
     print(count_pat(fname, ip))
-    br = ''
+    br = 'Chrome|Firefox|MSIE'
     print(count_pat(fname, br))
     fname = '/etc/passwd'
-    shell = ''
+    shell = 'bash$|nologin$'
     print(count_pat(fname, shell))
