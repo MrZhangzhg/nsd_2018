cfa17e2fb105421c1c4dcc456f318cb953ce6f21
MrZhangzhg <zhangzg@tedu.cn>
2019-03-22 14:14:49 +0800

    1810 devops day01 14:10

---
+++ nsd1810/devops/day01/zb3.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+print('start...')
+retval = os.fork()
+if retval:
+    print('父进程')
+    time.sleep(5)
+    result = os.waitpid(-1, 1)
+    print(result)
+    time.sleep(15)
+    print('父进程，结束')
+else:
+    print('子进程')
+    time.sleep(10)
+    print('子进程，结束')
+    exit()
+
