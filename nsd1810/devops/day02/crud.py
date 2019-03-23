1861ae4e067258c691664eb5a4ef7e5f84da3b07
MrZhangzhg <zhangzg@tedu.cn>
2019-03-23 11:32:51 +0800

    1810 devops day02 11:30

--- nsd1810/devops/day02/crud.py
+++ nsd1810/devops/day02/crud.py
@@ -9,11 +9,24 @@ conn = pymysql.connect(
     charset='utf8'
 )
 cursor = conn.cursor()
+############################################
+# insert1 = 'INSERT INTO department VALUES (%s, %s)'
+# cursor.execute(insert1, (1, '人事部'))
+# cursor.executemany(insert1, [(2, '运维部')])
+# cursor.executemany(insert1, [(3, '开发部'), (4, '财务部'), (5, '行政部'), (6, '市场部'), (7, '测试部')])
+############################################
+select1 = 'SELECT * FROM department order by dep_id'
+cursor.execute(select1)
+result = cursor.fetchone()
+print(result)
+print('*' * 30)
+result2 = cursor.fetchmany(2)
+print(result2)
+print('*' * 30)
+result3 = cursor.fetchall()
+print(result3)
 
-insert1 = 'INSERT INTO department VALUES (%s, %s)'
-cursor.execute(insert1, (1, '人事部'))
-cursor.executemany(insert1, [(2, '运维部')])
-cursor.executemany(insert1, [(3, '开发部'), (4, '财务部')])
+
 
 
 conn.commit()
