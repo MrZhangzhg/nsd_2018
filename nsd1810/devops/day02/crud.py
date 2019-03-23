382c685580c591842a42b6a1d4ab41e974ee5ffd
MrZhangzhg <zhangzg@tedu.cn>
2019-03-23 11:44:02 +0800

    1810 devops day02 11:40

--- nsd1810/devops/day02/crud.py
+++ nsd1810/devops/day02/crud.py
@@ -15,17 +15,26 @@ cursor = conn.cursor()
 # cursor.executemany(insert1, [(2, '运维部')])
 # cursor.executemany(insert1, [(3, '开发部'), (4, '财务部'), (5, '行政部'), (6, '市场部'), (7, '测试部')])
 ############################################
-select1 = 'SELECT * FROM department order by dep_id'
-cursor.execute(select1)
+# select1 = 'SELECT * FROM department order by dep_id'
+# cursor.execute(select1)
+# result = cursor.fetchone()
+# print(result)
+# print('*' * 30)
+# result2 = cursor.fetchmany(2)
+# print(result2)
+# print('*' * 30)
+# result3 = cursor.fetchall()
+# print(result3)
+############################################
+select2 = 'SELECT * FROM department order by dep_id'
+cursor.execute(select2)
+cursor.scroll(4)   # 默认以相对方式，从当前位置向下移动
 result = cursor.fetchone()
 print(result)
 print('*' * 30)
-result2 = cursor.fetchmany(2)
+cursor.scroll(0, mode='absolute')  # absolute一定是从开头移动
+result2 = cursor.fetchone()
 print(result2)
-print('*' * 30)
-result3 = cursor.fetchall()
-print(result3)
-
 
 
 
