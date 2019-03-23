e66200ab53eb91cdc2cc05c29968826bb291ea2f
MrZhangzhg <zhangzg@tedu.cn>
2019-03-23 14:49:12 +0800

    1810 devops day02 14:50

--- nsd1810/devops/day02/dbconn.py
+++ nsd1810/devops/day02/dbconn.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
@@ -17,6 +17,25 @@ class Departments(Base):
 
     def __str__(self):
         return '%s: %s' % (self.dep_id, self.dep_name)
+
+class Employees(Base):
+    __tablename__ = 'employees'
+    emp_id = Column(Integer, primary_key=True)
+    emp_name = Column(String(20))
+    birth_date = Column(Date)
+    email = Column(String(50))
+    dep_id = Column(Integer, ForeignKey('departments.dep_id'))
+
+    def __str__(self):
+        return self.emp_name
+
+class Salary(Base):
+    __tablename__ = 'salary'
+    auto_id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    emp_id = Column(Integer, ForeignKey('employees.emp_id'))
+    basic = Column(Integer)
+    awards = Column(Integer)
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
