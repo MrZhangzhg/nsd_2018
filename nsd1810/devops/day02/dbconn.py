e87ca00ea437b11ad68d5d81be06d942b09574a6
MrZhangzhg <zhangzg@tedu.cn>
2019-03-23 14:33:24 +0800

    1810 devops day02 14:30

--- nsd1810/devops/day02/dbconn.py
+++ nsd1810/devops/day02/dbconn.py
@@ -1,10 +1,22 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
     # 用户名:密码@服务器/数据库?参数
     'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu1810?charset=utf8',
     encoding='utf8',
-    echo=True  # 在屏幕终端输出详细日志，生产环境不要设置
+    # echo=True  # 在屏幕终端输出详细日志，生产环境不要设置
 )
+Base = declarative_base()
 
 
+class Departments(Base):
+    __tablename__ = 'departments'   # 类对应的表名
+    dep_id = Column(Integer, primary_key=True)
+    dep_name = Column(String(20), unique=True, nullable=False)
+
+    def __str__(self):
+        return '%s: %s' % (self.dep_id, self.dep_name)
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
