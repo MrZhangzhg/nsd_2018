9054f351126c17cae26f95969ddf25c718c4953d
MrZhangzhg <zhangzg@tedu.cn>
2019-03-25 11:29:39 +0800

    1810 devops day03 11:30

--- nsd1810/devops/day03/get_imgs.py
+++ nsd1810/devops/day03/get_imgs.py
@@ -1,5 +1,6 @@
 from urllib import request
 import re
+import os
 
 def download(url, fname):
     r = request.urlopen(url)
@@ -23,7 +24,15 @@ def get_url(fname, patt, encoding=None):
 if __name__ == '__main__':
     net_url = 'http://www.163.com/'
     net_fname = '/tmp/163.html'
-    download(net_url, net_fname)
+    download(net_url, net_fname) # 下载网易首页文件
     img_patt = '(http|https)://[-\w/.]+\.(jpg|gif|png|jpeg)'
-    print(get_url(net_fname, img_patt, 'gbk'))
-
+    img_url = get_url(net_fname, img_patt, 'gbk')  # 所有图片网址
+    # 创建目标目录
+    img_dir = '/tmp/net163/'
+    if not os.path.isdir(img_dir):
+        os.mkdir(img_dir)
+    # 下载图片
+    for url in img_url:
+        fname = url.split('/')[-1]  # 从URL中切出文件名
+        fname = os.path.join(img_dir, fname)  # 拼接绝对路径
+        download(url, fname)
