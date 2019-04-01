9221227c1ca33e16bfe6e760a8ac60f4bcb9836f
MrZhangzhg <zhangzg@tedu.cn>
2019-04-01 15:37:44 +0800

    1810 devweb day04 15:30

--- nsd1810/devweb/website/mysite/polls/urls.py
+++ nsd1810/devweb/website/mysite/polls/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # url(URL的正则, 对应的函数, url的名称)
     url(r'^$', views.index, name='index'),
     url(r'(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'(?P<question_id>\d+)/result/$', views.result, name='result'),
 ]
