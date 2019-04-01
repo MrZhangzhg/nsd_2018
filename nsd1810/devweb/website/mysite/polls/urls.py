bcb07a4217820e50bda13ad1e951e7f3ea00e4f0
MrZhangzhg <zhangzg@tedu.cn>
2019-04-01 14:51:44 +0800

    1810 devweb day04 14:50

--- nsd1810/devweb/website/mysite/polls/urls.py
+++ nsd1810/devweb/website/mysite/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # url(URL的正则, 对应的函数, url的名称)
     url(r'^$', views.index, name='index'),
+    url(r'(?P<question_id>\d+)/$', views.detail, name='detail'),
 ]
