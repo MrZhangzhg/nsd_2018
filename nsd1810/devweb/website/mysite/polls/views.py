efc77b527053e5928b18cfbf42371cb10443cb85
MrZhangzhg <zhangzg@tedu.cn>
2019-04-02 14:54:02 +0800

    nsd1810 py devweb day04 14:50

--- nsd1810/devweb/website/mysite/polls/views.py
+++ nsd1810/devweb/website/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
 
 def index(request):
@@ -11,3 +11,11 @@ def detail(request, question_id):
 
 def result(request, question_id):
     return render(request, 'result.html', {'question_id': question_id})
+
+def vote(request, question_id):
+    choice_id = request.POST.get('choice_id')
+    question = Question.objects.get(id=question_id)
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+    return redirect('result', question_id=question.id)
