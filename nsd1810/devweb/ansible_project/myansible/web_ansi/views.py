231bffca7490e848d0124290fff990e4ece6d2c7
MrZhangzhg <zhangzg@tedu.cn>
2019-04-03 17:27:09 +0800

    nsd1810 py devweb day05 17:30

--- nsd1810/devweb/ansible_project/myansible/web_ansi/views.py
+++ nsd1810/devweb/ansible_project/myansible/web_ansi/views.py
@@ -8,5 +8,14 @@ def mainpage(request):
     return render(request, 'mainpage.html')
 
 def addhosts(request):
+    if request.method == 'POST':
+        group_name = request.POST.get('group').strip()
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group_name:  # 如果group不是空串
+            group = HostGroup.objects.get_or_create(group_name=group_name)[0]
+            if host and ip:  # 如果host和ip也都不是空串
+                group.host_set.get_or_create(hostname=host, ipaddr=ip)
+
     groups = HostGroup.objects.all()
     return render(request, 'addhosts.html', {'groups': groups})
