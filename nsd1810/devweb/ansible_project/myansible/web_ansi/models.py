e848bc5480287babfb7b8fbb32d333d04734a1b4
MrZhangzhg <zhangzg@tedu.cn>
2019-04-03 10:20:39 +0800

    nsd1810 py devweb day05 10:20

--- nsd1810/devweb/ansible_project/myansible/web_ansi/models.py
+++ nsd1810/devweb/ansible_project/myansible/web_ansi/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class HostGroup(models.Model):
+    group_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.group_name
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=100, unique=True)
+    ipaddr = models.CharField(max_length=15)
+    group = models.ForeignKey(HostGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s:%s=> %s' % (self.hostname, self.ipaddr, self.group)
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.module_name
+
+class Argument(models.Model):
+    args_text = models.CharField(max_length=200, unique=True)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s=> %s" % (self.module, self.args_text)
