28ef26f87c3eba1ad4ee0ef3f2e9617f12d7dfd1
MrZhangzhg <zhangzg@tedu.cn>
2019-04-04 13:29:04 +0800

    nsd1810 py devweb day06 13:30

--- nsd1810/devweb/ansible_project/myansible/web_ansi/views.py
+++ nsd1810/devweb/ansible_project/myansible/web_ansi/views.py
@@ -1,5 +1,49 @@
 from django.shortcuts import render
 from .models import HostGroup, Module, Host
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
+
+def ad_hoc(inventory_fname, hosts, module, args):
+    Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+    options = Options(connection='ssh', module_path=[''], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+    loader = DataLoader()
+    passwords = dict()
+    inventory = InventoryManager(loader=loader, sources=inventory_fname)
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    play_source=dict(
+            name="my ansible play",
+            hosts=hosts,
+            gather_facts='no',
+            tasks=[
+                dict(action=dict(module=module, args=args), register='shell_out'),
+                # dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
+             ]
+        )
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+                  inventory=inventory,
+                  variable_manager=variable_manager,
+                  loader=loader,
+                  options=options,
+                  passwords=passwords,
+              )
+        result = tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+
+        shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+
+
 
 def index(request):
     return render(request, 'index.html')
@@ -43,6 +87,8 @@ def tasks(request):
             dest = group_name
         else:
             dest = ip
+
+        ad_hoc(['ansi_cfg/dhosts.py'], dest, module_name, args_text)
 
     groups = HostGroup.objects.all()
     modules = Module.objects.all()
