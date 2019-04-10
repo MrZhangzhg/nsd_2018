b41d44bc6a0300091fb23cc923f59daaadf9ae2b
MrZhangzhg <zhangzg@tedu.cn>
2019-04-10 20:28:02 +0800

    1811 python01 day01 18:00

--- nsd1811/python01/day01/hi.py
+++ nsd1811/python01/day01/hi.py
@@ -1,2 +1,13 @@
-print()
+# print('Hello World!')
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
 
+# print('Hello World')
+# print('Hello', 'World!', 'hao', 123)  # 默认打印的各项之间用空格分隔
+# print('Hello', 'World!', 'hao', 123, sep='***')  # 打印各项间用***分开
+# print('Hello' + 'World' + '!')   # 字符串拼接
+
+num = input('number: ')
+print(num)
