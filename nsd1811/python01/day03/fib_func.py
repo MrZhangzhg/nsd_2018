e06035d3bd34a1e0267b977600d298440f6909fc
MrZhangzhg <zhangzg@tedu.cn>
2019-04-12 16:56:37 +0800

    nsd1811 python01 day03 16:55

--- nsd1811/python01/day03/fib_func.py
+++ nsd1811/python01/day03/fib_func.py
@@ -5,7 +5,10 @@ def gen_fib():
     for i in range(n - 2):
         fib.append(fib[-1] + fib[-2])
 
-    print(fib)
+    # print(fib)
+    return fib   # 函数运算的结果需要使用return返回值，否则返回None
 
-gen_fib()
-gen_fib()
+mylist = gen_fib()
+print(mylist)
+new_list = [10 + i for i in mylist]
+print(new_list)
