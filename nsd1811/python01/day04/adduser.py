bd1c07499f33444c67a2faacab1eb71560da399e
MrZhangzhg <zhangzg@tedu.cn>
2019-04-13 17:08:49 +0800

    nsd1811 python01 day04 17:10

--- nsd1811/python01/day04/adduser.py
+++ nsd1811/python01/day04/adduser.py
@@ -1,8 +1,19 @@
 import sys
+import subprocess
 from randpass2 import gen_pass
 
 def adduser(user, passwd, fname):
-
+    info = '''用户信息：
+用户名: %s
+密码: %s
+''' % (user, passwd)
+    subprocess.run('useradd %s' % user, shell=True)
+    subprocess.run(
+        'echo %s | passwd --stdin %s' % (passwd, user),
+        shell=True
+    )
+    with open(fname, 'a') as fobj:
+        fobj.write(info)
 
 if __name__ == '__main__':
     username = sys.argv[1]
