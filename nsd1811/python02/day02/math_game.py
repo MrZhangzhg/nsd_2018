3176932ce16e92bb72a993f00c9b77048257d0e1
MrZhangzhg <zhangzg@tedu.cn>
2019-04-16 14:51:44 +0800

    nsd1811 python02 day02 14:50

---
+++ nsd1811/python02/day02/math_game.py
@@ -0,0 +1,13 @@
+def exam():
+
+
+def main():
+    while True:
+        exam()
+        yn = input('Continue(y/n)? ').strip()[0]
+        if yn in 'nN':
+            print('\nBye-bye')
+            break
+
+if __name__ == '__main__':
+    main()
