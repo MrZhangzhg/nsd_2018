c3a5d47652186c69cf5b522feed4ed88d368a2fc
MrZhangzhg <zhangzg@tedu.cn>
2019-04-16 15:21:40 +0800

    nsd1811 python02 day02 15:20

--- nsd1811/python02/day02/math_game.py
+++ nsd1811/python02/day02/math_game.py
@@ -1,5 +1,20 @@
-def exam():
+from random import randint, choice
 
+def exam():
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列，默认升序
+    op = choice('+-')
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])  # 算式
+    answer = int(input(prompt))
+    if answer == result:
+        print('Very good!!!')
+    else:
+        print('不对哦')
 
 def main():
     while True:
