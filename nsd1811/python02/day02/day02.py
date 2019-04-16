a7d258ac6f8b42b552c8fa4135c6d686edee3db2
MrZhangzhg <zhangzg@tedu.cn>
2019-04-16 14:37:26 +0800

    nsd1811 python02 day02 14:40

--- nsd1811/python02/day02/day02.py
+++ nsd1811/python02/day02/day02.py
@@ -1,8 +1,14 @@
-def foo():
-    print('in foo')
-    bar()
+def func1(*args):
+    # args前面的*号，表明args是元组，传参会把参数放入元组
+    print(args)
 
-def bar():
-    print('in bar')
+def func2(**kwargs):
+    # kwargs前面的**号，表明kwargs是字典
+    print(kwargs)
 
-foo()
+if __name__ == '__main__':
+    func1()
+    func1('hao')
+    func1('hao', 123)
+    func2()
+    func2(name='tom', age=20)
