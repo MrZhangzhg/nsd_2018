699c54ab66b5cefb0f06043c9e03a6609b2bf074
MrZhangzhg <zhangzg@tedu.cn>
2019-04-16 16:30:01 +0800

    nsd1811 python02 day02 16:30

--- nsd1811/python02/day02/day02.py
+++ nsd1811/python02/day02/day02.py
@@ -1,14 +1,34 @@
-def func1(*args):
-    # args前面的*号，表明args是元组，传参会把参数放入元组
-    print(args)
-
-def func2(**kwargs):
-    # kwargs前面的**号，表明kwargs是字典
-    print(kwargs)
+# def func1(*args):
+#     # args前面的*号，表明args是元组，传参会把参数放入元组
+#     print(args)
+#
+# def func2(**kwargs):
+#     # kwargs前面的**号，表明kwargs是字典
+#     print(kwargs)
+#
+# if __name__ == '__main__':
+#     func1()
+#     func1('hao')
+#     func1('hao', 123)
+#     func2()
+#     func2(name='tom', age=20)
+##########################################
+def add(x, y):
+    return x + y
 
 if __name__ == '__main__':
-    func1()
-    func1('hao')
-    func1('hao', 123)
-    func2()
-    func2(name='tom', age=20)
+    print(add(10, 5))
+    myadd = lambda x, y: x + y
+    print(myadd(10, 20))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nsd1811/python02/day02/math_game3.py
@@ -0,0 +1,43 @@
+from random import randint, choice
+
+def exam():
+    cmds = {'+': lambda x, y: x + y, '-': lambda x, y: x - y}
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列，默认升序
+    op = choice('+-')
+    result = cmds[op](*nums)   # 将列表拆开
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])  # 算式
+    counter = 0
+
+    while counter < 3:
+        try:
+            answer = int(input(prompt))
+        except:  # 可以捕获所有异常，但是不推荐
+            print()
+            continue
+
+        if answer == result:
+            print('Very good!!!')
+            break
+
+        print('不对哦')
+        counter += 1
+    else:
+        print('%s%s' % (prompt, result))
+
+def main():
+    while True:
+        exam()
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
+        if yn in 'nN':
+            print('\nBye-bye')
+            break
+
+if __name__ == '__main__':
+    main()
