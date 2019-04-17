370eabfd73656e1b7dd57e2e5ac7e5a4c887c3f9
MrZhangzhg <zhangzg@tedu.cn>
2019-04-17 15:31:48 +0800

    nsd1811 python02 day03 15:30

--- nsd1811/python02/day03/backup.py
+++ nsd1811/python02/day03/backup.py
@@ -1,13 +1,26 @@
+import os
+import tarfile
+from check_md5 import check_md5
 from time import strftime
 
-def full_backup():
+def full_backup(src, dst, md5file):
+    fname = os.path.basename(src)
+    fname = '%s_full_%s.tar.gz' % (fname, strftime('%Y%m%d'))
+    tar = tarfile.open(fname, 'w:gz')
+    tar.add(src)
+    tar.close()
+
+    md5dict = {}
 
 
-def incr_backup():
+def incr_backup(src, dst, md5file):
 
 
 if __name__ == '__main__':
+    src = '/tmp/mydemo/security'
+    dst = '/tmp/demo'
+    md5file = '/tmp/demo/md5.data'
     if strftime('%a') == 'Mon':
-        full_backup()
+        full_backup(src, dst, md5file)
     else:
-        incr_backup()
+        incr_backup(src, dst, md5file)
