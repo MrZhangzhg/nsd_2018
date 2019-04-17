b29affeacb793b9714e1dee734205f515b369b0c
MrZhangzhg <zhangzg@tedu.cn>
2019-04-17 10:41:56 +0800

    nsd1811 python02 day03 10:40

--- nsd1811/python02/day03/colors.py
+++ nsd1811/python02/day03/colors.py
@@ -7,6 +7,7 @@ def deco(func):
 def hello():
     return 'Hello World!'
 
+@deco
 def greet():
     return 'ni hao'
 
@@ -14,3 +15,4 @@ if __name__ == '__main__':
     print(hello())
     a = deco(hello)   # 将hello函数作为参数传递给deco，deco的返回值是red函数
     print(a())  # 调用a函数，实际上是调用red函数
+    print(greet())
