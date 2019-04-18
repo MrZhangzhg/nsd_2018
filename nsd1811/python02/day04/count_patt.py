acd1307b5c90d6901b15d4186bc85c54710fc016
MrZhangzhg <zhangzg@tedu.cn>
2019-04-18 17:47:51 +0800

    nsd1811 python02 day04 17:52

--- nsd1811/python02/day04/count_patt.py
+++ nsd1811/python02/day04/count_patt.py
@@ -1,10 +1,19 @@
+import re
 
 def count_patt(fname, patt):
-
+    patt_dict = {}
+    cpatt = re.compile(patt)
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:  # 如果匹配到内容，更新字典，None表示False
+                key = m.group()
+                patt_dict[key] = patt_dict.get(key, 0) + 1
+    return patt_dict
 
 if __name__ == '__main__':
     fname = 'access_log'
-    ip = ''
-    br = ''
+    ip = '^(\d+\.){3}\d+'  # 1.11.123.45,  12345.11111.23423.2535234
+    br = 'Chrome|Firefox|MSIE'
     print(count_patt(fname, ip))
     print(count_patt(fname, br))
