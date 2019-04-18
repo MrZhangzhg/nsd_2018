6c1482858818e5b32cb1e6dd6956a5ea53a323bf
MrZhangzhg <zhangzg@tedu.cn>
2019-04-18 11:32:37 +0800

    nsd1811 python02 day04 11:30

---
+++ nsd1811/python02/day04/game4.py
@@ -0,0 +1,14 @@
+class Warrior:
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+
+    def speak(self, content):
+        print('我是%s，%s' % (self.name, content))
+
+class MaleWarrior(Warrior):  # 括号中指定父类(基类)
+    pass
+
+if __name__ == '__main__':
+    lb = MaleWarrior('吕布', '方天画㦸')
+    lb.speak('aaaa')
