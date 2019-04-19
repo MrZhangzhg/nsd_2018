eda68eeab672b8fb22ac523e019755b7031a2247
MrZhangzhg <zhangzg@tedu.cn>
2019-04-19 11:53:56 +0800

    nsd1811 devops day01 11:50

--- nsd1811/devops/day01/tcp_serv2.py
+++ nsd1811/devops/day01/tcp_serv2.py
@@ -7,15 +7,21 @@ s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(1)
-cli_sock, cli_addr = s.accept()
-print('客户机: ', cli_addr)
+
 while True:
-    data = cli_sock.recv(1024)
-    if data.strip() == b'quit':
+    try:
+        cli_sock, cli_addr = s.accept()
+    except KeyboardInterrupt:
+        print()
         break
-    print(data.decode(), end='')
-    cli_sock.send(b'How are you?\r\n')
-cli_sock.close()
+    print('客户机: ', cli_addr)
+    while True:
+        data = cli_sock.recv(1024)
+        if data.strip() == b'quit':
+            break
+        print(data.decode(), end='')
+        sdata = input('> ') + '\r\n'  # input读入的是str类型
+        cli_sock.send(sdata.encode())  # 转成bytes类型发送
+    cli_sock.close()
+
 s.close()
-
-
