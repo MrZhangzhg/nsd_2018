d445a7c1633990528731e91c5851f57af0a27982
MrZhangzhg <zhangzg@tedu.cn>
2019-04-19 11:00:13 +0800

    nsd1811 devops day01 11:00

--- nsd1811/devops/day01/tcp_serv.py
+++ nsd1811/devops/day01/tcp_serv.py
@@ -4,6 +4,8 @@ host = ''   # 表示0.0.0.0
 port = 12345
 addr = (host, port)
 s = socket.socket()  # 默认创建TCP套接字
+# 程序执行结束后，操作系统默认会保留套接字1分钟，在1分钟内无法再次使用相同的套接字
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 取消1分钟限制
 s.bind(addr)  # 绑定地址到套接字
 s.listen(1)  # 启动监听过程  netstat -tlnp | grep :12345
 cli_sock, cli_addr = s.accept()  # 等待客户端连接，收到连接返回(客户机套接字，客户机地址)
