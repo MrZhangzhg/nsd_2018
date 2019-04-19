67bed320d87b866c159ca1c2baba32935f4119b5
MrZhangzhg <zhangzg@tedu.cn>
2019-04-19 15:37:34 +0800

    nsd1811 devops day01 15:40

--- nsd1811/devops/day01/udp_client.py
+++ nsd1811/devops/day01/udp_client.py
@@ -5,6 +5,13 @@ port = 12345
 addr = (host, port)  # 待连接的服务器地址
 c = socket.socket(type=socket.SOCK_DGRAM)
 
-c.sendto(b'ni hao\r\n', addr)
-info = c.recvfrom(1024)
-print(info)
+while True:
+    data = input('> ') + '\r\n'
+    if data.strip() == 'quit':
+        break
+    c.sendto(data.encode(), addr)
+    # info = c.recvfrom(1024)   # (数据，服务器地址)
+    data = c.recvfrom(1024)[0]
+    print(data.decode(), end='')
+
+c.close()
--- nsd1811/devops/day01/udp_serv.py
+++ nsd1811/devops/day01/udp_serv.py
@@ -1,4 +1,5 @@
 import socket
+from time import strftime
 
 host = ''
 port = 12345
@@ -7,9 +8,17 @@ s = socket.socket(type=socket.SOCK_DGRAM)  # UDP的type是SOCK_DGRAM
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 
-data, cli_addr = s.recvfrom(1024)  # 一次最多接收1024字节数据，返回值是(数据，客户机地址)
-print(data.decode(), end='')
-s.sendto(b'Hello World\r\n', cli_addr)  # 向客户机地址发送数据
+
+while True:
+    try:
+        data, cli_addr = s.recvfrom(1024)  # 一次最多接收1024字节数据，返回值是(数据，客户机地址)
+    except KeyboardInterrupt:
+        print()
+        break
+    data = data.decode()  # bytes转str
+    print(data, end='')
+    rdata = '[%s] %s' % (strftime('%H:%M:%S'), data)
+    s.sendto(rdata.encode(), cli_addr)  # 向客户机地址发送数据
 
 s.close()
 
