85ae3483a934c6708054c25427c19f0596b99082
MrZhangzhg <zhangzg@tedu.cn>
2019-04-22 17:49:00 +0800

    nsd1811 devops day02 18:00

--- nsd1811/devops/day02/py_mysql.py
+++ nsd1811/devops/day02/py_mysql.py
@@ -10,8 +10,22 @@ conn = pymysql.connect(
     charset='utf8'
 )
 cursor = conn.cursor()  # 创建游标
-create_dep = ''   # SQL语句
-cursor.execute(create_dep)   # 执行sql语句
+# SQL语句
+# create_dep = '''CREATE TABLE departments(
+# dep_id INT, dep_name VARCHAR(20),
+# PRIMARY KEY(dep_id)
+# )'''
+# cursor.execute(create_dep)   # 执行sql语句
+# create_emps = '''CREATE TABLE employees(
+# emp_id INT, emp_name VARCHAR(20), email VARCHAR(50), dep_id INT,
+# PRIMARY KEY(emp_id), FOREIGN KEY(dep_id) REFERENCES departments(dep_id)
+# )'''
+# cursor.execute(create_emps)
+create_sal = '''CREATE TABLE salary(
+auto_id INT, emp_id INT, date DATE, basic INT, awards INT,
+PRIMARY KEY(auto_id), FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+)'''
+cursor.execute(create_sal)
 conn.commit()    # 提交改动
 cursor.close()   # 关闭游标
 conn.close()     # 关闭连接
