b3018b8cb48e0e6e351b6b01c199c3cdb0f6c775
MrZhangzhg <zhangzg@tedu.cn>
2019-04-22 15:58:39 +0800

    nsd1811 devops day02 16:00

--- nsd1811/devops/day02/timeit.py
+++ nsd1811/devops/day02/timeit.py
@@ -9,23 +9,23 @@ def myadd():
     print(result)
 
 if __name__ == '__main__':
-    # start1 = time.time()
-    # for i in range(2):
-    #     myadd()
-    # end1 = time.time()
-    # print(end1 - start1)
+    start1 = time.time()
+    for i in range(2):
+        myadd()
+    end1 = time.time()
+    print(end1 - start1)
 ###################################
-    # start2 = time.time()
-    # for i in range(2):
-    #     retval = os.fork()
-    #     if not retval:
-    #         myadd()
-    #         exit()
-    # # 因为创建了两个子进程，每个waitpid只能监听一个子进程，挂起两次要执行两次waitpid
-    # for i in range(2):
-    #     os.waitpid(-1, 0)
-    # end2 = time.time()
-    # print(end2 - start2)
+    start2 = time.time()
+    for i in range(2):
+        retval = os.fork()
+        if not retval:
+            myadd()
+            exit()
+    # 因为创建了两个子进程，每个waitpid只能监听一个子进程，挂起两次要执行两次waitpid
+    for i in range(2):
+        os.waitpid(-1, 0)
+    end2 = time.time()
+    print(end2 - start2)
 ###################################
     start3 = time.time()
     t1 = threading.Thread(target=myadd)
