03c6e787ee55a3fad53dd7b8e1c5a10a69d41352
MrZhangzhg <zhangzg@tedu.cn>
2019-04-22 11:50:59 +0800

    nsd1811 devops day02 11:50

--- nsd1811/devops/day02/mt1.py
+++ nsd1811/devops/day02/mt1.py
@@ -1,14 +1,14 @@
 import threading
 import time
 
-def say_hi():
+def say_hi(n=3):
     print('Hello World!')
-    time.sleep(3)
+    time.sleep(n)
     print('Done')
 
 if __name__ == '__main__':
     print('start')
     t1 = threading.Thread(target=say_hi)  # 创建工作线程
     t1.start()  # 启动工作线程，将会执行target() => say_hi()
-    t2 = threading.Thread(target=say_hi)
-    t2.start()
+    t2 = threading.Thread(target=say_hi, args=(7,))
+    t2.start()  # target(*args)  => target(7) => say_hi(7)
