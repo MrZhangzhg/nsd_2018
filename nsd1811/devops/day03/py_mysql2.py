d3e7c673988d4c5957268ffc7b70fce2c81b5835
MrZhangzhg <zhangzg@tedu.cn>
2019-04-23 09:31:28 +0800

    nsd1811 devops day03 9:30

--- nsd1811/devops/day03/py_mysql2.py
+++ nsd1811/devops/day03/py_mysql2.py
@@ -12,7 +12,17 @@ cursor = conn.cursor()
 ##############################
 insert_dep1 = 'INSERT INTO departments VALUES (%s, %s)'
 deps = [(1, '人事部'), (2, '运维部'), (3, '开发部'), (4, '市场部')]
-cursor.executemany(insert_dep1, deps)
+# cursor.executemany(insert_dep1, deps)
+
+deps2 = [(5, '测试部')]
+# cursor.executemany(insert_dep1, deps2)
+
+deps3 = [(6, '财务部'), (7, '运营部')]
+# cursor.executemany(insert_dep1, deps3)
+##############################
+update_hr = 'UPDATE departments SET dep_name=%s WHERE dep_name=%s'
+data = ('人力资源部', '人事部')
+cursor.execute(update_hr, data)
 ##############################
 conn.commit()    # 提交改动
 cursor.close()   # 关闭游标
