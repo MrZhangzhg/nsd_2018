0f15a2328c3c9cbe3f88d8593c79d5d61403f9e1
MrZhangzhg <zhangzg@tedu.cn>
2019-04-23 09:55:38 +0800

    nsd1811 devops day03 10:00

--- nsd1811/devops/day03/py_mysql2.py
+++ nsd1811/devops/day03/py_mysql2.py
@@ -28,16 +28,26 @@ del_yy = 'DELETE FROM departments WHERE dep_name=%s'
 yy_dep = ('运营部',)
 # cursor.execute(del_yy, yy_dep)
 ##############################
-select1 = 'SELECT * from departments ORDER BY dep_id'
-cursor.execute(select1)
-result1 = cursor.fetchone()  # 取一行记录
-result2 = cursor.fetchmany(2)   # 取2行记录
-result3 = cursor.fetchall()   # 取全部数据
+# select1 = 'SELECT * from departments ORDER BY dep_id'
+# cursor.execute(select1)
+# result1 = cursor.fetchone()  # 取一行记录
+# result2 = cursor.fetchmany(2)   # 取2行记录
+# result3 = cursor.fetchall()   # 取全部数据
+# print(result1)
+# print('#' * 30)
+# print(result2)
+# print('#' * 30)
+# print(result3)
+##############################
+select2 = 'SELECT * from departments ORDER BY dep_id'
+cursor.execute(select2)
+cursor.scroll(3, mode='absolute')  # 以绝对方式向下移动3条记录
+result1 = cursor.fetchone()
+cursor.scroll(-1)  # 默认以相对方式移动
+result2 = cursor.fetchone()
 print(result1)
 print('#' * 30)
 print(result2)
-print('#' * 30)
-print(result3)
 ##############################
 conn.commit()    # 提交改动
 cursor.close()   # 关闭游标
