5e4958c663d383962b036d06f2e8f9035d2669ab
MrZhangzhg <zhangzg@tedu.cn>
2019-04-25 09:57:41 +0800

    nsd1811 devops day04 10:00

--- nsd1811/devops/day04/download.py
+++ nsd1811/devops/day04/download.py
@@ -1,5 +1,6 @@
 from urllib import request
 import re
+import os
 
 def download(url, fname):
     r = request.urlopen(url)
@@ -26,4 +27,10 @@ if __name__ == '__main__':
     download(url163, fname163)
     img_patt = 'http://[\w/.-]+\.(jpg|png|jpeg|gif)'
     img_urls = get_patt(fname163, img_patt, 'gbk')
-    print(img_urls)
+    img_dir = '/tmp/163'
+    if not os.path.exists(img_dir):
+        os.mkdir(img_dir)
+    for url in img_urls:
+        img_fname = url.split('/')[-1]   # 取出网址中的文件名
+        img_fname = os.path.join(img_dir, img_fname)
+        download(url, img_fname)
