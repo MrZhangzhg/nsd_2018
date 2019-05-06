01454526a0957575ab55b4bb25a18c143d751c62
MrZhangzhg <zhangzg@tedu.cn>
2019-05-06 11:42:04 +0800

    nsd1811 devweb day05 11:40

--- nsd1811/devweb/day04/mysite/polls/views.py
+++ nsd1811/devweb/day04/mysite/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Question
 
 # 用户发送请求时，将会把请求作为第一个参数传递
 def index(request):
-    return render(request, 'index.html')
+    questions = Question.objects.order_by('-pub_date')
+    return render(request, 'index.html', {'questions': questions})
 
 def detail(request, question_id):
     return render(request, 'detail.html', {'question_id': question_id})
