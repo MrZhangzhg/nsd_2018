83fa152a4ed4741d47f7bf034c14a7825847984f
MrZhangzhg <zhangzg@tedu.cn>
2019-05-07 09:46:01 +0800

    py 1811 devweb day06 9:40

--- nsd1811/devweb/ansible_project/myansible/myansible/settings.py
+++ nsd1811/devweb/ansible_project/myansible/myansible/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$z2sivo-p-8vx2+ua@x1^(h(dma@imy=l6rj&)4h+xn(2j-=!z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webansi',
 ]
 
 MIDDLEWARE = [
@@ -103,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
