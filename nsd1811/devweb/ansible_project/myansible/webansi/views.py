cd30f446a85fda6afb29f231ff7d0b61bb708a70
MrZhangzhg <zhangzg@tedu.cn>
2019-05-08 17:48:20 +0800

    py 1811 devweb day07 17:50

--- nsd1811/devweb/ansible_project/myansible/webansi/views.py
+++ nsd1811/devweb/ansible_project/myansible/webansi/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render, redirect
 from .models import HostGroup, Module, Argument, Host
 
+import shutil
+from collections import namedtuple
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+import ansible.constants as C
+
+def adhoc(sources, hosts, module, args):
+    Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+    options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+    loader = DataLoader()
+    passwords = dict()
+    inventory = InventoryManager(loader=loader, sources=sources)
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    play_source =  dict(
+            name = "Ansible Play",
+            hosts = hosts,
+            gather_facts = 'no',
+            tasks = [
+                dict(action=dict(module=module, args=args), register='shell_out'),
+             ]
+        )
+
+    play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+                  inventory=inventory,
+                  variable_manager=variable_manager,
+                  loader=loader,
+                  options=options,
+                  passwords=passwords,
+              )
+        result = tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+
+        shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
+
+
+
 def index(request):
     return render(request, 'index.html')
 
@@ -39,6 +83,18 @@ def delarg(request, arg_id):
     return redirect('addmodules')
 
 def tasks(request):
+    if request.method == 'POST':
+        hostname = request.POST.get('host')
+        groupname = request.POST.get('group')
+        module = request.POST.get('module')
+        arg = request.POST.get('argument')
+        if hostname:
+            dest = hostname
+        else:
+            dest = groupname
+
+        adhoc(['ansicfg/dhosts.py'], dest, module, arg)
+
     groups = HostGroup.objects.all()
     modules = Module.objects.all()
     hosts = Host.objects.all()
