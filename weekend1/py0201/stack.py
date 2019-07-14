cdeb9d94b8b615c49ddf20bc251e2914c71a0961
MrZhangzhg <zhangzg@tedu.cn>
2019-07-14 14:08:54 +0800

    weekend py0201 14:02

--- weekend1/py0201/stack.py
+++ weekend1/py0201/stack.py
@@ -15,6 +15,7 @@ def view_it():
     print(stack)
 
 def show_menu():
+    # 将函数存到字典中
     cmds = {'0': push_it, '1': pop_it, '2': view_it}
     prompt = """(0) push
 (1) pop
@@ -23,7 +24,6 @@ def show_menu():
 Please input your choice(0/1/2/3): """
 
     while True:
-        # 将函数存到字典中
         choice = input(prompt).strip()  # 去除用户输出的额外的空格
         if choice not in ['0', '1', '2', '3']:
             print('Invalid choice. Try again.')
@@ -33,7 +33,7 @@ Please input your choice(0/1/2/3): """
             print('Bye-bye')
             break
 
-        cmds[choice]()
+        cmds[choice]()   # 在字典中找到对应的函数，进行调用
 
         # if choice == '0':
         #     push_it()
